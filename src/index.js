17dbb9124dab03bc89cb8d52778c22da01283321
root <root@ip-172-31-22-174.ec2.internal>
2020-02-24 23:52:27 +0000

    Added CORS module to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bcrypt = require("bcryptjs")
+const cors = require("cors")
 require("./db/mongoose")
 const userRouter = require('./routers/user')
 const taskRouter = require("./routers/task")
@@ -9,6 +10,7 @@ const app = express()
 
 /*	SETUP EXPRESS ROUTERS	*/
 app.use(express.json())
+app.use(cors())
 app.use(userRouter)
 app.use(taskRouter)
     
